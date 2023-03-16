e54e65abde2749cde2e326314e107735a07f85df
yoshizawa-code <73457180+yoshizawa-code@users.noreply.github.com>
2023-03-16 15:41:24 +0900

    Add files via upload

--- api_list.py
+++ api_list.py
@@ -1,2 +1,2 @@
 DEV_API_LIST = ["http://127.0.0.1:8002", "http://127.0.0.1:8003"]
-PRD_API_LIST = ["", ""]
+PRD_API_LIST = ["https://render2-kmep.onrender.com/", "https://render3-qru6.onrender.com/"]
